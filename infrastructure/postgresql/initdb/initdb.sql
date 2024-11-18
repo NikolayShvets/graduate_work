9d93c63e0c97934b4a8594967ee91cd36dbadff2
Alena Markina <markina.a7@yandex.ru>
2024-11-18 18:53:02 +0100

    feat: добавлена инфраструктура проекта

--- infrastructure/postgresql/initdb/initdb.sql
+++ infrastructure/postgresql/initdb/initdb.sql
@@ -1,4 +1,2 @@
-CREATE DATABASE movies_auth;
-CREATE DATABASE movies_consumer;
-GRANT ALL PRIVILEGES ON DATABASE movies_auth TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE movies_consumer TO postgres;
+CREATE DATABASE auth;
+GRANT ALL PRIVILEGES ON DATABASE auth TO auth;
